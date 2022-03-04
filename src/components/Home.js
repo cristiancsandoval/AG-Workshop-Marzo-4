a4f1a6b702d30b7ffa9d5c36f59d322825f74852
cristiancsandoval <clinares668@gmail.com>
2022-03-04 10:41:15 -0500

    home y estilos

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,31 @@
 import React from 'react'
 
 const Home = () => {
+
   return (
-    <div>Home</div>
+    <div>
+        <form>
+            <div>
+                <label>Marca</label>
+                <input type="text"></input>
+            </div>
+            <div>
+                <label>Color</label>
+                <input type="text"></input>
+            </div>
+            <div>
+                <label>Ruta foto</label>
+                <input type="text"></input>
+            </div>
+            <button>Enviar</button>
+        </form>
+        <div>
+            <h2>Bicis</h2>
+            <div>
+                {}
+            </div>
+        </div>
+    </div>
   )
 }
 
