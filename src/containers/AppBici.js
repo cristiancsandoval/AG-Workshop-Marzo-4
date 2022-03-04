1ba7c80a069c9b5c09b3ce5da2dbd9b644081dbc
cristiancsandoval <clinares668@gmail.com>
2022-03-04 11:10:44 -0500

    router

--- src/containers/AppBici.js
+++ src/containers/AppBici.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import RouterBici from '../Router/RouterBici'
 
 const AppBici = () => {
   return (
     <div>
       <Navbar/>
+      <RouterBici/>
     </div>
   )
 }
